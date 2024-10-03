7fa96b5d564debe0076e6857edc0edbbc52accb7
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-10-03 11:08:22 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Amisha"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Amisha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Amisha" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
