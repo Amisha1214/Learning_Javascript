b46fe87d59b39546438c788a3d1a6d51f4ac6eee
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-08 11:23:53 +0530

    Comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,20 +1,39 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+// Comparison Operators
+var num1 = 10;
+var num2 = 20;
+var num3 = 10;
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+var str1 = "10";
+var str2 = "20";
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// 1. Equal to
+var result = num1 == num2; //false
+var result = num1 == num3; //true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// 2. Strict Equal
+var result = num1 == num1; //false
+var result = num1 == num3; //true
 
-// ===
+// 3. Not Equal
+var result = num1 != num2; //true
+var result = num1 != num3; //false
 
-console.log("2" === 2);
+// 4. Strict Not Equal
+var result = num1 !== num2; //false
+var result = num1 !== num3; //true
+
+// 5. Greater than
+var result = num1 > num2; //false
+var result = num2 > num3; //true
+
+// 6. Grater than or Equal to
+var result = num1 >= num3; //true
+var result = num2 >= num3; //false
+
+// 7. Lesser than
+var result = num1 < num3; //false
+var result = num2 < num3; //true
+
+// 8. Lesser than or Equal to
+var result = num1 <= num3; //false
+var result = num2 <= num3; //true
