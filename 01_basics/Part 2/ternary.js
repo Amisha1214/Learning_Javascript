ec2f0a7fbab7c6894cd582d13e4f238a7578645c
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-09 20:51:36 +0530

    Use of Ternary

---
+++ 01_basics/Part 2/ternary.js
@@ -0,0 +1,19 @@
+let isUserLoggedIn = false
+let isTheCoursePurchased = true
+if (isUserLoggedIn) {
+    console.log("Welcome to the website");
+}
+else{
+    console.log("Please Login");
+}
+isUserLoggedIn ? console.log("Welcome to the website") :
+console.log("Please login")
+ 
+
+
+isUserLoggedIn
+    ? isTheCoursePurchased
+    ?console.log("Access Granted")
+    :console.log("Access Denied !! Please Buy The Course")
+    :console.log("Access Denied !! Please login")
+
