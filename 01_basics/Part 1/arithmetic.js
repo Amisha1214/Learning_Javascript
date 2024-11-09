6a8403fd5166774619318b43b0be8530350283ee
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-09 19:20:18 +0530

    Arithmetic

---
+++ 01_basics/Part 1/arithmetic.js
@@ -0,0 +1,37 @@
+//  Arithmetic Operators
+var num1 = 100;
+var num2 = 2;
+
+// 1. Addition
+var result = num1 + num2; //102
+
+// 2. Subtraction
+var result = num1 - num2; //98
+
+// 3. Multiplication
+var result = num1 * num2; //200
+
+// 4. Division
+var result = num1 + num2; //50
+
+// 5. Modulus
+var result = num1 % num2; //0
+
+// 6. Exponential
+var result = num1 ** num2; //1000
+
+// 7. Pre Increment
+var num = 10;
+var result = ++num; // num= 11; result = 11
+
+// 8. Post Increment
+var num = 10;
+var result = num++; // num= 10; result = 11
+
+// 9. Pre Decrement
+var num = 10;
+var result = --num; // num= 9; result = 9
+
+// 10. Post Decrement
+var num = 10;
+var result = num--; // num= 10; result = 9
