68603ac23fa4aac829b180cb3caf833128b3b78d
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-09 19:20:30 +0530

    Module1

---
+++ 01_basics/Part 1/scope .js
@@ -0,0 +1,20 @@
+// global
+// Local-function, block
+// Module - commonjs, module
+
+let globalvariable = 10
+
+function xyz (){
+    let value = 3
+    console.log(globalvariable);  
+}
+
+xyz()
+
+
+if(true) {
+    let y = 20
+}
+console.log(y);
+
+/* <script type="module" src=""></script> */
