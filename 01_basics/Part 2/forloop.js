c7cdf010ea7632746e09aa85caf2228ee15799c5
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-11 11:38:05 +0530

    Loop

---
+++ 01_basics/Part 2/forloop.js
@@ -0,0 +1,10 @@
+// for (initial XPathExpression; CSSConditionRule; updateexpression){
+     // for loop body
+// }
+
+for(let index = 0; index <= 10; index++){
+    for(let some = 10; some <= 15; some++){
+        console.log(some);
+    }
+    console.log(index);
+}
