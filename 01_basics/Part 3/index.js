31ebcbea291bfab42c9e8ca9ab218c3c3a71d9aa
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-12 20:35:35 +0530

    Introduction To Functions

--- 01_basics/Part 3/index.js
+++ 01_basics/Part 3/index.js
@@ -1,55 +1,53 @@
-// let e = 10;
-// let f = 20;
-// let g = 30;
-// let h = 40;
-// console.log(a + b);
-// console.log(c + d);
+// Variables and basic addition
+let e = 10;
+let f = 20;
+let g = 30;
+let h = 40;
+console.log(e + f); // Outputs: 30
+console.log(g + h); // Outputs: 70
 
-
-/* Function */
+// Function without parameters, using global variables a and b
 let a = 10;
 let b = 20;
+
 function add(){
-    // block of code
-    console.log(a + b);
-}
-add() //function calling
-
- // Parameter
-function add(a,b){
-    // block of code
-    console.log(a + b);
-}
-add(20, 30) //function calling
-add(5, 3)
- // argument
-
-let sum = function add(a, b){
-    // block of code
-    console.log(a + b);
+    console.log(a + b); // Outputs: 30
 }
 
-console.log(sum(10,20));
+add(); // Calls the add function using global variables a and b
 
+// Function with parameters
+function add(a, b){
+    console.log(a + b); // Outputs the sum of the parameters passed
+}
 
-// first class sitizen
+add(20, 30); // Outputs: 50
+add(5, 3);   // Outputs: 8
 
+// Function expression assigned to the variable sum
+let sum = function(a, b){
+    return a + b; // Returns the sum of the parameters passed
+};
+
+console.log(sum(10, 20)); // Outputs: 30
+
+// First-class functions: function x takes a function as an argument
 function x(callback){
-    console.log('Hello');
-    callback()
+    console.log('Hello');  // Outputs: Hello
+    callback();            // Calls the callback function
 }
 
 function y(){
-    console.log('Bye');
+    console.log('Bye');    // Outputs: Bye
 }
-x(y)
 
+x(y); // Calls function y as a callback after printing Hello
+
+// Function with return statement
 function add(a, b){
-    // block of code
-    return a + b;
-    console.log('hello');
-    console.log(1000);
+    return a + b; // Returns the sum of a and b
 }
-let sum = add(10, 20) //function calling
 
-console.log(sum);
+let sumResult = add(10, 20); // Stores the result of add(10, 20)
+
+console.log(sumResult); // Outputs: 30
