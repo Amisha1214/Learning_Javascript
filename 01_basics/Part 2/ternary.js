a2225bfed6daaa7815dcf1b012bb2f32282aca6d
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-14 10:30:14 +0530

    Intro to ternary

--- 01_basics/Part 2/ternary.js
+++ 01_basics/Part 2/ternary.js
@@ -1,18 +1,25 @@
+// Boolean variable to check if the user is logged in
 let isUserLoggedIn = false
+// Boolean variable to check if the user has purchased the course
 let isTheCoursePurchased = true
+
+// Basic if-else condition to check if the user is logged in
 if (isUserLoggedIn) {
-    console.log("Welcome to the website");
+    console.log("Welcome to the website");  // If user is logged in, display welcome message
 }
 else{
     console.log("Please Login");
 }
-isUserLoggedIn ? console.log("Welcome to the website"):
-console.log("Please login")
+// Using a ternary operator to check if the user is logged in
+isUserLoggedIn 
+? console.log("Welcome to the website"):    // If true, display welcome message
+console.log("Please login") // If false, prompt them to log in
  
 
-
+// Nested ternary operator to check both login and purchase status
 isUserLoggedIn
     ? isTheCoursePurchased
-    ?console.log("Access Granted")
-    :console.log("Access Denied !! Please Buy The Course")
-    :console.log("Access Denied !! Please login")
+    ?console.log("Access Granted")      // If logged in and course is purchased, grant access
+    : console.log("Access Denied !! Please Buy The Course")     // If logged in but course not purchased, deny access with message
+    : console.log("Access Denied !! Please login");     // If not logged in, deny access and prompt to log in
+    
