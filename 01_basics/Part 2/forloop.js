210600df39c3e38bc477a4f196848bd13a19368e
Amisha Achari <136420068+Amisha1214@users.noreply.github.com>
2024-11-14 10:29:35 +0530

    Intro to Loop

--- 01_basics/Part 2/forloop.js
+++ 01_basics/Part 2/forloop.js
@@ -2,9 +2,11 @@
      // for loop body
 // }
 
+// Outer loop: Initializes index to 0 and runs until index is less than or equal to 10
 for(let index = 0; index <= 10; index++){
+        // Inner loop: Initializes some to 10 and runs until some is less than or equal to 15
     for(let some = 10; some <= 15; some++){
-        console.log(some);
+        console.log(some);      // Logs the current value of some in each iteration of the inner loop
     }
-    console.log(index);
+    console.log(index);     // Logs the current value of index after the inner loop completes
 }
